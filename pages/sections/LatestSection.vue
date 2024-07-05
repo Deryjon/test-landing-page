cc1915fc29caae8f8b15eb1b8461339da1b0e479
Dery Jon <yusupoviskandar1005@gmail.com>
2024-07-05 12:21:55 +0500

    feat: add changes

--- pages/sections/LatestSection.vue
+++ pages/sections/LatestSection.vue
@@ -28,9 +28,18 @@ const news = [
             <p class="text-center w-[500px] mx-auto text-[16px] mt-[20px]">Lorem ipsum dolor sit amet, consectetur
                 adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
 
-                <div class="wrapper flex flex-wrap justify-between mt-[60p]">
-                    <div class="card w-[410px] h-[410px]" v-for="card in news">
-                        <img :src='card.photo' alt="">
+                <div class="wrapper flex flex-wrap justify-between mt-[60px]">
+                    <div class="card w-[410px] h-[410px] relative " v-for="card in news">
+                        <img :src='card.photo' alt="" class="rounded-[15px]">
+                        <div class="texts absolute bottom-[10px] left-[10px] w-[390px] flex items-center justify-between  bg-[#FFFFFF66] px-[15px] py-[20px]">
+                            <div class="date-name w-[250px] text-white">
+                                <p>{{ card.data }}</p>
+                                <p class="text-[18px] font-extrabold">{{ card.name }}</p>
+                            </div>
+                            <div class="arrow text-white">
+<img src="../../assets/icons/white-arrow.svg" alt="">
+                            </div>
+                        </div>
                     </div>
                 </div>
         </div>
